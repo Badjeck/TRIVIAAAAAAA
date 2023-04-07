f5377847411e57e296feac7737484e0096bc001e
Terencyl <36328154+Terencyl@users.noreply.github.com>
2023-04-07 15:36:03 +0200

    Fixed rock questions replaced by techno using add instead of addPlayer (#12)
    * Fixed rock questions replaced by techno using add instead of addPlayer
    
    * Fixed missing parameter is setIsTechnoQuestionsEnabled

--- src/Utils/ConsoleSpy.ts
+++ src/Utils/ConsoleSpy.ts
@@ -2,7 +2,6 @@ import {IConsole} from "./IConsole";
 
 export class ConsoleSpy implements IConsole{
     public content: string = "";
-
     log(message: string) {
         this.content += message;
     }
